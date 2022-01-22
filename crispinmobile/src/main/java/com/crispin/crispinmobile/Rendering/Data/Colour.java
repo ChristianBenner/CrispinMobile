dccd3abcf0f72df4f694ec29b7cdacd6bda1c06b
Christian Benner <christianbenner35@gmail.com>
2022-01-22 15:58:42 +0000

    Default colours added

---
+++ crispinmobile/src/main/java/com/crispin/crispinmobile/Rendering/Data/Colour.java
@@ -0,0 +1,41 @@
+package com.crispin.crispinmobile.Rendering.Data;
+
+import glm_.vec4.Vec4;
+
+public class Colour {
+    // Pre-defined red colour
+    public static final Vec4 RED = new Vec4(1.0f, 0.0f, 0.0f, 1.0f);
+
+    // Pre-defined green colour
+    public static final Vec4 GREEN = new Vec4(0.0f, 1.0f, 0.0f, 1.0f);
+
+    // Pre-defined blue colour
+    public static final Vec4 BLUE = new Vec4(0.0f, 0.0f, 1.0f, 1.0f);
+
+    // Pre-defined black colour
+    public static final Vec4 BLACK = new Vec4(0.0f, 0.0f, 0.0f, 1.0f);
+
+    // Pre-defined white colour
+    public static final Vec4 WHITE = new Vec4(1.0f, 1.0f, 1.0f, 1.0f);
+
+    // Pre-defined grey colour
+    public static final Vec4 GREY = new Vec4(0.5f, 0.5f, 0.5f, 1.0f);
+
+    // Pre-defined light-grey colour
+    public static final Vec4 LIGHT_GREY = new Vec4(0.75f, 0.75f, 0.75f, 1.0f);
+
+    // Pre-defined dark-grey colour
+    public static final Vec4 DARK_GREY = new Vec4(0.25f, 0.25f, 0.25f, 1.0f);
+
+    // Pre-defined yellow colour
+    public static final Vec4 YELLOW = new Vec4(1.0f, 1.0f, 0.0f, 1.0f);
+
+    // Pre-defined cyan colour
+    public static final Vec4 CYAN = new Vec4(0.0f, 1.0f, 1.0f, 1.0f);
+
+    // Pre-defined magenta colour
+    public static final Vec4 MAGENTA = new Vec4(1.0f, 0.0f, 1.0f, 1.0f);
+
+    // Pre-defined orange colour
+    public static final Vec4 ORANGE = new Vec4(1.0f, 0.415f, 0.0f, 1.0f);
+}
