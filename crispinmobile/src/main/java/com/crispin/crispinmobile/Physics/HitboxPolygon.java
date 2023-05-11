ee407f4d037d5340e25499d1e89185e137816fcf
chrisbenner <christianbenner35@gmail.com>
2023-05-11 22:23:28 +0100

    Slight improvements to collision code

--- crispinmobile/src/main/java/com/crispin/crispinmobile/Physics/HitboxPolygon.java
+++ crispinmobile/src/main/java/com/crispin/crispinmobile/Physics/HitboxPolygon.java
@@ -8,12 +8,19 @@ import com.crispin.crispinmobile.Rendering.Utilities.ModelMatrix;
 public class HitboxPolygon {
     private float[] data;
     private float[] transformedPoints;
-    private Line[] lines;
     private float[] axes;
+
+    // A reference to the points that should be used (either data or transformedPoints)
+    private float[] points;
+
+    // These lines are only initialised when attempting to render for the first time. Rendering will
+    // likely only occur in debugging scenarios to show hitboxes in development.
+    private Line[] lines;
 
     // Assumes two components (xy)
     public HitboxPolygon(float[] data) {
         this.data = data;
+        this.points = data;
 
         // Allocate now so it does not need to be allocate every collision calculation
         this.axes = new float[data.length];
@@ -24,6 +31,7 @@ public class HitboxPolygon {
         if(transformedPoints == null) {
             // Allocate now so it does not need to be allocate every transform call (expensive)
             this.transformedPoints = new float[data.length];
+            this.points = transformedPoints;
         }
 
         float[] m = modelMatrix.getFloats();
@@ -37,23 +45,18 @@ public class HitboxPolygon {
     }
 
     private void calculateAxes() {
-        float[] points = transformedPoints == null ? data : transformedPoints;
-
         // Calculate axes for this polygon
         for (int i = 0; i < points.length; i += 2) {
             int next = (i + 2) % points.length;
             float edgeX = points[next] - points[i];
             float edgeY = -(points[next + 1] - points[i + 1]);
-            float length = (float) Math.sqrt(Math.abs((edgeY
-                    * edgeY) + (edgeX * edgeX)));
+            float length = (float) Math.sqrt(Math.abs((edgeY * edgeY) + (edgeX * edgeX)));
             axes[i] = edgeY / length;
             axes[i + 1] = edgeX / length;
         }
     }
 
     public void render(Camera2D camera2D) {
-        float[] points = transformedPoints == null ? data : transformedPoints;
-
         if (lines == null) {
             // Initialise lines for debugging
             this.lines = new Line[points.length / 2];
@@ -72,26 +75,23 @@ public class HitboxPolygon {
 
     // Using SAT collision algorithm
     public boolean isCollidingSAT(HitboxPolygon other) {
-        float[] points = transformedPoints == null ? data : transformedPoints;
-        float[] otherPoints = other.transformedPoints == null ? other.data : other.transformedPoints;
-
-        float[] proj1 = new float[2];
-        float[] proj2 = new float[2];
+        float[] projectionMyAxis = new float[2];
+        float[] projectionOtherAxis = new float[2];
 
         // Collision check using this polygons axes
         for (int i = 0; i < axes.length; i += 2) {
-            projectPolygon(proj1, axes[i], axes[i + 1], points);
-            projectPolygon(proj2, axes[i], axes[i + 1], otherPoints);
-            if (!overlap(proj1, proj2)) {
+            projectPolygon(projectionMyAxis, axes[i], axes[i + 1], points);
+            projectPolygon(projectionOtherAxis, axes[i], axes[i + 1], other.points);
+            if (!overlap(projectionMyAxis, projectionOtherAxis)) {
                 return false;
             }
         }
 
         // Collision check using other polygons axes
         for (int i = 0; i < other.axes.length; i += 2) {
-            projectPolygon(proj1, other.axes[i], other.axes[i + 1], points);
-            projectPolygon(proj2, other.axes[i], other.axes[i + 1], otherPoints);
-            if (!overlap(proj1, proj2)) {
+            projectPolygon(projectionMyAxis, other.axes[i], other.axes[i + 1], points);
+            projectPolygon(projectionOtherAxis, other.axes[i], other.axes[i + 1], other.points);
+            if (!overlap(projectionMyAxis, projectionOtherAxis)) {
                 return false;
             }
         }
